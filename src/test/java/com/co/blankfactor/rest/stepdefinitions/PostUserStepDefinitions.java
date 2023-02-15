03cf504c922af66be5270ced4c9a043538fa1c88
Jefferson Martinez <jefferson.martinez@blankfactor.com>
2023-02-15 11:35:20 -0500

    post user stepdefinitions created

---
+++ src/test/java/com/co/blankfactor/rest/stepdefinitions/PostUserStepDefinitions.java
@@ -0,0 +1,16 @@
+package com.co.blankfactor.rest.stepdefinitions;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+
+public class PostUserStepDefinitions {
+    @Given("^that Blank want to create a new user with Post method$")
+    public void thatBlankWantToCreateANewUserWithPostMethod() {
+
+    }
+
+    @Then("^a new user was created$")
+    public void aNewUserWasCreated() {
+
+    }
+}
