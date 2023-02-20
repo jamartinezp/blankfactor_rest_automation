6ea384337ae32215c25ff03e8fbc75f9f2ee14e3
Jefferson Martinez <jefferson.martinez@blankfactor.com>
2023-02-20 10:18:37 -0500

    Incorrect expected valies exception added

--- src/main/java/com/co/blankfactor/rest/exceptions/IncorrectExpectedValuesException.java
+++ src/main/java/com/co/blankfactor/rest/exceptions/IncorrectExpectedValuesException.java
@@ -1,2 +1,8 @@
-package com.co.blankfactor.rest.exceptions;public class IncorrectExpectedValues {
+package com.co.blankfactor.rest.exceptions;
+
+public class IncorrectExpectedValuesException extends AssertionError{
+    public static final String INCORRECT_EXPECTED_VALUES =
+            "VALUES EXPECTED ARE INCORRECT";
+
+    public IncorrectExpectedValuesException(String message, Throwable e) { super(message, e);}
 }
