0259c35aed413cee7b7bdfb0a4279c9691a12ac4
Jefferson Martinez <jefferson.martinez@blankfactor.com>
2023-02-23 11:14:46 -0500

    PutUser method modified

--- src/main/java/com/co/blankfactor/rest/questions/VerifyUserUpdated.java
+++ src/main/java/com/co/blankfactor/rest/questions/VerifyUserUpdated.java
@@ -1,2 +1,37 @@
-package com.co.blankfactor.rest.questions;public class VerifyUserUpdated {
+package com.co.blankfactor.rest.questions;
+
+import com.co.blankfactor.rest.models.User;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Question;
+
+import java.util.Map;
+
+import static com.co.blankfactor.rest.tasks.PostUser.SESSION_USER_VARIABLE;
+import static com.co.blankfactor.rest.utils.JsonUtilities.convertJson;
+import static net.serenitybdd.rest.SerenityRest.lastResponse;
+import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isPresent;
+
+public class VerifyUserUpdated implements Question<Boolean> {
+    private Map convertedJson;
+    private User userData;
+
+    public static VerifyUserUpdated ofTheResponse() {
+        return new VerifyUserUpdated();
+    }
+
+    @Override
+    public Boolean answeredBy(Actor actor) {
+        convertedJson = convertJson(lastResponse().getBody().prettyPrint());
+        userData = actor.recall(SESSION_USER_VARIABLE);
+        return verifyUserData() && verifyInformationField();
+    }
+
+    private boolean verifyUserData(){
+        return userData.getName().equals(convertedJson.get("name"))
+                && userData.getJobTitle().equals(convertedJson.get("jobTitle"));
+    }
+
+    private boolean verifyInformationField(){
+        return convertedJson.containsKey("updatedAt");
+    }
 }
--- src/test/java/com/co/blankfactor/rest/stepdefinitions/PutUserStepDefinitions.java
+++ src/test/java/com/co/blankfactor/rest/stepdefinitions/PutUserStepDefinitions.java
@@ -1,16 +1,39 @@
 package com.co.blankfactor.rest.stepdefinitions;
 
+import com.co.blankfactor.rest.exceptions.IncorrectExpectedValuesException;
+import com.co.blankfactor.rest.exceptions.IncorrectResponseCodeException;
+import com.co.blankfactor.rest.questions.ResponseCode;
+import com.co.blankfactor.rest.questions.VerifyUserUpdated;
+import com.co.blankfactor.rest.tasks.PutUser;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
+import net.serenitybdd.screenplay.rest.abilities.CallAnApi;
+import net.thucydides.core.util.EnvironmentVariables;
+
+import static com.co.blankfactor.rest.exceptions.IncorrectExpectedValuesException.INCORRECT_EXPECTED_VALUES;
+import static com.co.blankfactor.rest.exceptions.IncorrectResponseCodeException.INCORRECT_RESPONSE_CODE;
+import static com.co.blankfactor.rest.models.builders.UserBuilder.with;
+import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
+import static net.serenitybdd.screenplay.actors.OnStage.*;
+import static org.hamcrest.Matchers.equalTo;
 
 public class PutUserStepDefinitions {
-    @Given("^that Blank want to update an user with id (\\d+), sending morpheus as new name and zone as new job$")
-    public void thatBlankWantToUpdateAnUserWithIdSendingMorpheusAsNewNameAndZoneAsNewJob(int arg1) {
-
+    private EnvironmentVariables environmentVariables;
+    @Given("^that (.*) want to update an user with id (.*), sending (.*) as new name and (.*) as new job$")
+    public void thatBlankWantToUpdateAnUserWithIdSendingMorpheusAsNewNameAndZoneAsNewJob
+            (String actorName, int id, String newName, String newJob) {
+        theActorCalled(actorName).whoCan(CallAnApi.at(environmentVariables.getProperty("api.rest.baseUrl")))
+                .attemptsTo(PutUser.inTheSystem(id,with().theName(newName).andJobTitle(newJob)));
     }
 
     @Then("^he see that an user was updated$")
     public void heSeeThatAnUserWasUpdated() {
+        theActorInTheSpotlight()
+                .should(seeThat(ResponseCode.was(), equalTo(200))
+                        .orComplainWith(IncorrectResponseCodeException.class, INCORRECT_RESPONSE_CODE ));
 
+        theActorInTheSpotlight()
+                .should(seeThat(VerifyUserUpdated.ofTheResponse())
+                        .orComplainWith(IncorrectExpectedValuesException.class, INCORRECT_EXPECTED_VALUES ));
     }
 }
