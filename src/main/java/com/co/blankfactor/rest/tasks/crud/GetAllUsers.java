3403413484e6a73982e756428e993c3676de9482
Jefferson Martinez <jefferson.martinez@blankfactor.com>
2023-03-06 09:47:25 -0500

    Get method tasks updated

--- src/main/java/com/co/blankfactor/rest/tasks/crud/GetAllUsers.java
+++ src/main/java/com/co/blankfactor/rest/tasks/crud/GetAllUsers.java
@@ -1,2 +1,28 @@
-package com.co.blankfactor.rest.tasks.crud;public class GetAllUsers {
+package com.co.blankfactor.rest.tasks.crud;
+
+import io.restassured.http.ContentType;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.Tasks;
+import net.serenitybdd.screenplay.rest.interactions.Get;
+
+import static com.co.blankfactor.rest.questions.ResponseToken.RECALL_TOKEN;
+
+public class GetAllUsers implements Task {
+    private static final String ENDPOINT = "/api/unknown";
+
+    public static GetAllUsers inTheSystem() {
+        return Tasks.instrumented(GetAllUsers.class);
+    }
+
+    @Override
+    public <T extends Actor> void performAs(T actor) {
+        actor.attemptsTo(
+                Get.resource(ENDPOINT)
+                        .with(requestSpecification
+                                -> requestSpecification
+                                .contentType(ContentType.JSON)
+                                .header("token", RECALL_TOKEN)));
+
+    }
 }
