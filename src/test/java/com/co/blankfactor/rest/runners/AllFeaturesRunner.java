7bb564f206e65a0d57d33c37fc388c4122a1ffef
Jefferson Martinez <jefferson.martinez@blankfactor.com>
2023-03-06 09:46:26 -0500

    AllFeaturesRunner class added

--- src/test/java/com/co/blankfactor/rest/runners/AllFeaturesRunner.java
+++ src/test/java/com/co/blankfactor/rest/runners/AllFeaturesRunner.java
@@ -1,2 +1,15 @@
-package com.co.blankfactor.rest.runners;public class AllFeaturesRunner {
+package com.co.blankfactor.rest.runners;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = {"com.co.blankfactor.rest"},
+        snippets = SnippetType.CAMELCASE
+)
+public class AllFeaturesRunner {
 }
