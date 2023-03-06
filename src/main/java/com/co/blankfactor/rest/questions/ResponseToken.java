fcabc49ccc1ac161f63399efd43edb1c5d019b99
Jefferson Martinez <jefferson.martinez@blankfactor.com>
2023-03-06 09:48:04 -0500

    Questions fixed

--- src/main/java/com/co/blankfactor/rest/questions/ResponseToken.java
+++ src/main/java/com/co/blankfactor/rest/questions/ResponseToken.java
@@ -1,2 +1,26 @@
-package com.co.blankfactor.rest.questions;public class ResponseToken {
+package com.co.blankfactor.rest.questions;
+
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Question;
+
+import java.util.Map;
+
+import static com.co.blankfactor.rest.utils.JsonUtilities.convertJson;
+import static net.serenitybdd.rest.SerenityRest.lastResponse;
+
+public class ResponseToken implements Question<String> {
+    private Map convertedJson;
+    private static final String TOKEN = "TOKEN";
+    public static String RECALL_TOKEN;
+
+    public static ResponseToken wasSave() {
+        return new ResponseToken();
+    }
+
+    @Override
+    public String answeredBy(Actor actor) {
+        convertedJson = convertJson(lastResponse().getBody().prettyPrint());
+        actor.remember(TOKEN, convertedJson.get("token"));
+        return RECALL_TOKEN = actor.recall(TOKEN);
+    }
 }
--- src/main/java/com/co/blankfactor/rest/questions/VerifyFieldToken.java
+++ src/main/java/com/co/blankfactor/rest/questions/VerifyFieldToken.java
@@ -8,17 +8,14 @@ import java.util.Map;
 import static com.co.blankfactor.rest.utils.JsonUtilities.convertJson;
 import static net.serenitybdd.rest.SerenityRest.lastResponse;
 
-public class ResponseToken implements Question<Boolean> {
-    private Map convertedJson;
-    public static final String TOKEN = "TOKEN";
-    public static  String RECALL_TOKEN = "";
+public class VerifyFieldToken implements Question<String> {
 
-    public static ResponseToken exists() {
-        return new ResponseToken();
+    public static VerifyFieldToken exists() {
+        return new VerifyFieldToken();
     }
+
     @Override
-    public Boolean answeredBy(Actor actor) {
-        convertedJson = convertJson(lastResponse().getBody().prettyPrint());
-        return convertedJson.containsKey("tken");
+    public String answeredBy(Actor actor) {
+        return lastResponse().getBody().prettyPrint();
     }
 }
